e43ef8d0349ba2a74a2948b564154bf0dc6e1e79
virajv_webelight <virajv@webelight.co.in>
2024-05-20 15:04:24 +0530

    Configure mobile app as mqtt client

--- app/src/main/java/com/webelight/mqttpoc/MainActivity.kt
+++ app/src/main/java/com/webelight/mqttpoc/MainActivity.kt
@@ -1,18 +1,58 @@
 package com.webelight.mqttpoc
 
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.IMqttActionListener
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.IMqttToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private lateinit var myMqttClient: MyMqttClient
+    private lateinit var messageTextView: TextView
+    private lateinit var messageEditText: EditText
+    private lateinit var publishButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, MqttService::class.java)
-        startService(intent)
+
+        messageTextView = findViewById(R.id.messageTextView)
+        messageEditText = findViewById(R.id.messageEditText)
+        publishButton = findViewById(R.id.publishButton)
+
+        myMqttClient = MyMqttClient()
+
+        myMqttClient.subscribeToTopic("test/topic")
+
+        publishButton.setOnClickListener {
+            val message = messageEditText.text.toString()
+            if (message.isNotEmpty()) {
+                myMqttClient.publishMessage("test/topic", message)
+            }
+        }
+
+        myMqttClient.setMessageCallback { topic, message ->
+            runOnUiThread {
+                messageTextView.text = message
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/webelight/mqttpoc/MyMqttClient.kt
@@ -0,0 +1,71 @@
+package com.webelight.mqttpoc
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.widget.Toast
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+
+class MyMqttClient {
+
+    private val brokerUrl = "tcp://broker.hivemq.com:1883"
+    private val clientId = "webelight_solution"
+    private var mqttClient: MqttClient? = null
+
+    init {
+        try {
+            mqttClient = MqttClient(brokerUrl, clientId, MemoryPersistence())
+            mqttClient?.setCallback(object : MqttCallback {
+                override fun connectionLost(cause: Throwable?) {
+
+                }
+
+                override fun messageArrived(topic: String?, message: MqttMessage?) {
+                    message?.let {
+                        messageCallback?.invoke(topic ?: "", it.toString())
+                    }
+                }
+
+                override fun deliveryComplete(token: IMqttDeliveryToken?) {
+
+                }
+            })
+
+            val options = MqttConnectOptions()
+            options.isCleanSession = true
+            mqttClient?.connect(options)
+            Log.d("fuck you", "connection done")
+
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun publishMessage(topic: String, message: String) {
+        try {
+            val mqttMessage = MqttMessage(message.toByteArray())
+            mqttClient?.publish(topic, mqttMessage)
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun subscribeToTopic(topic: String) {
+        try {
+            mqttClient?.subscribe(topic)
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    private var messageCallback: ((String, String) -> Unit)? = null
+
+    fun setMessageCallback(callback: (String, String) -> Unit) {
+        messageCallback = callback
+    }
+}
